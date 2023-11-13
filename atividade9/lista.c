e8dac4dd1444602c16ae586adfe589fa1c793006
Lucas Félix <137567861+lucasfelixgit@users.noreply.github.com>
2023-11-13 15:22:56 -0300

    Update lista.c

--- atividade9/lista.c
+++ atividade9/lista.c
@@ -1,62 +1,49 @@
+// lista.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "lista.h"
 
-No* no(float valor, No* proximo_no){
-    No* no = malloc(sizeof(No));
-    no->valor = valor;
-    no->proximo_no = proximo_no;
-    return no;
+No* lista_criar() {
+    No* cabeca = (No*)malloc(sizeof(No));
+    cabeca->proximo_no = NULL;
+    return cabeca;
 }
 
-void lista_inserir_no(No* L, No* no){
-    if(L != NULL){
-        if(L->proximo_no == NULL)
-        {
-            L->proximo_no = no;
-        }
-        else
-        {
-            lista_inserir_no(L->proximo_no, no);
-        }
-    }
+void lista_inserir_no(No* L, float valor) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo_no = L->proximo_no;
+    L->proximo_no = novo_no;
 }
 
-
-void lista_inserir_no_ordenado(No** L, No* no){
-    if(*L != NULL){
-        if((*L)->valor >= no->valor)
-        {
-            no->proximo_no = *L;
-            *L = no;
-        } 
-        else if ((*L)->proximo_no != NULL) 
-        {
-            lista_inserir_no_ordenado(&(*L)->proximo_no, no);
-        } 
-        else 
-        {
-            (*L)->proximo_no = no;
-        }
+void lista_imprimir(No* L) {
+    No* atual = L->proximo_no;
+    while (atual != NULL) {
+        printf("%.2f ", atual->valor);
+        atual = atual->proximo_no;
     }
+    printf("\n");
 }
 
-void lista_imprimir(No* L){
-    if(L != NULL)
-    {
-        printf("%f ", L->valor);
-        lista_imprimir(L->proximo_no);
-    } 
-    else 
-    {
-        printf("error");
+void lista_liberar(No* L) {
+    while (L->proximo_no != NULL) {
+        No* no_remover = L->proximo_no;
+        L->proximo_no = no_remover->proximo_no;
+        free(no_remover);
     }
+    free(L);
 }
 
-void lista_liberar(No* L){
-    if(L != NULL)
-    {
-        lista_liberar(L->proximo_no);
-        free(L);
+void lista_inserir_no_ordenado(No* L, float valor) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo_no = NULL;
+
+    No* atual = L;
+    while (atual->proximo_no != NULL && atual->proximo_no->valor < valor) {
+        atual = atual->proximo_no;
     }
+    novo_no->proximo_no = atual->proximo_no;
+    atual->proximo_no = novo_no;
 }
