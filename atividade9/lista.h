8691b7b82dcd20d4d5ec26a9fa1d836da89c5a68
Lucas Félix <137567861+lucasfelixgit@users.noreply.github.com>
2023-11-13 15:24:34 -0300

    Update lista.h

--- atividade9/lista.h
+++ atividade9/lista.h
@@ -1,10 +1,15 @@
-typedef struct No{
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct No {
     float valor;
     struct No* proximo_no;
 } No;
 
-No* no(float valor, No* proximo_no);
-void lista_inserir_no(No* L, No* no);
-void lista_inserir_no_ordenado(No** L, No* no);
+No* lista_criar();
+void lista_inserir_no(No* L, float valor);
 void lista_imprimir(No* L);
 void lista_liberar(No* L);
+void lista_inserir_no_ordenado(No* L, float valor);
+
+#endif
